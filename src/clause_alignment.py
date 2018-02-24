fef4b936a6ef344fefd6fbf1f670c3da88c27a6c
Chong Ruan <ruanchong@rd.netease.com>
2018-02-25 00:39:22 +0800

    ADD: vicinity bonus.

--- src/clause_alignment.py
+++ src/clause_alignment.py
@@ -129,7 +129,7 @@ def calculate_score(word_vector, clause1, clause2):
             # 之后可以考虑利用 TF-IDF 等进行加权
             similarities.append((sim, i, j))
     similarities.sort()
-    print(similarities)
+    # print(similarities)
     similarity_values = [sim for sim, _1, _2 in similarities]
 
     top_k = 5
@@ -152,7 +152,7 @@ def calculate_score(word_vector, clause1, clause2):
         else:
             clause = clause1 + clause2
         # clause = clause.split()
-        average_index = sum([word_vector.vocab[w].index for w in clause])
+        average_index = sum([word_vector.vocab[w].index for w in clause]) / len(clause)
         if score > 0.5:
             # index -> 1, weight -> 0, score -> 0.5
             # index -> \inf, weight -> 1, score -> original score
@@ -173,7 +173,7 @@ def calculate_score(word_vector, clause1, clause2):
     #         rank_correlation = scipy.stats.spearmanr(similarity_is, similarity_js)[0]
     #
     #     # rank_correlation 的范围是 [-1, 1]
-    #     print("Spearman's rho: ", rank_correlation)
+    #     # print("Spearman's rho: ", rank_correlation)
     #
     #     # 如果 rank_correlation = 1，匹配分数不做折扣；
     #     # 如果 rank_correlation = 1，匹配分数折扣 0.9 倍；
@@ -188,7 +188,7 @@ def calculate_score(word_vector, clause1, clause2):
     # show_sentence(clause2)
     # print("Similarity = ", similarities)
     # print("Score = ", score)
-    return score
+    return score, len(clause1), len(clause2)
 
 
 def construct_and_solve_matching(score_matrix, D, verbose, problem_name="pre-matching"):
@@ -273,9 +273,15 @@ def align_sentence_pair(word_vector, s1, s2, D=3, verbose=False):
 
     # scores = np.random.random_integers(-2, 10, [l1, l2])
     scores = np.zeros([l1, l2], dtype=np.float32)
+    effective_lengths1 = np.zeros(l1, dtype=np.float32)
+    effective_lengths2 = np.zeros(l2, dtype=np.float32)
+
     for i in range(l1):
         for j in range(l2):
-            scores[i, j] = calculate_score(word_vector, s1_clauses[i], s2_clauses[j])
+            scores[i, j], el1, el2 = calculate_score(word_vector, s1_clauses[i], s2_clauses[j])
+            # 这里有重复计算，不过无所谓了
+            effective_lengths1[i] = el1
+            effective_lengths2[j] = el2
 
     # pre-matching
     matching_result = construct_and_solve_matching(scores, D, verbose, "pre-matching")
@@ -299,11 +305,25 @@ def align_sentence_pair(word_vector, s1, s2, D=3, verbose=False):
         for i in range(l1):
             for j in range(l2):
                 dist_to_diagonal = abs((i - range1[0]) * (range2[1] - range2[0]) - (range1[1] - range1[0]) * (j - range2[0]))
-                print("dist: ", i, j, dist_to_diagonal)
+                if verbose:
+                    print("dist: ", i, j, dist_to_diagonal)
                 # if (i - range1[0]) / (range1[1] - range1[0]) $\approx$ (j - range2[0]) / (range2[1] - range2[0]):
                 scores[i, j] += bonus * np.exp(-dist_to_diagonal)
                 if scores[i, j] > 1.0:
                     scores[i, j] = 1.0
+
+        # 确认匹配的位置附近的短句给奖励
+        threshold = 0.97
+        vicinity_range, vicinity_bonus = 3, 0.1
+        for i in range(l1):
+            for j in range(l2):
+                # 相邻位置有一个很确信的元素，本身又包含至少一个小句
+                if (i > 0 and scores[i-1, j] > threshold) or (j > 0 and scores[i, j-1] > threshold) \
+                        or (i+1 < l1 and scores[i+1, j] > threshold) or (j+1 < l2 and scores[i, j+1] > threshold):
+                    if effective_lengths1[i] <= vicinity_range or effective_lengths2[j] <= vicinity_range:
+                        scores[i, j] += vicinity_bonus
+                        if scores[i, j] > 1.0:
+                            scores[i, j] = 1.0
 
         matching_result = construct_and_solve_matching(scores, D, verbose, "refine-matching")
 
@@ -330,6 +350,128 @@ def align_all_corpus(word_vector, corpus1, corpus2, output_file):
                         print("Processed " + str(cnt) + " lines.\n\n")
 
 
+def unit_test():
+    verbose_option = False
+
+    sent1 = u"里德 太太 对此 则 完全 装聋作哑 ， 她 从 来看 不见 他 打 我 ， 也 从来 听不见 他 骂 我 ， 虽然 他 经常 当着 她 的 面 打 我 骂 我 。"
+    sent2 = u"里德 太太 呢 ， 在 这种 事情 上 ， 总是 装聋作哑 ， 她 从 来看 不见 他 打 我 ， 也 从来 听不见 他 骂 我 ， 虽然 他 常常 当着 她 的 面 既 打 我 又 骂 我 。"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    sent1 = u"“ 你 这个 狠毒 的 坏孩子 ！ ” 我 说 ， “ 你 简直 像 个 杀人犯 … … 你 是 个 管 奴隶 的 监工 … … 你 像 那班 罗马 暴君 ！ ” 我 看过 哥 尔德 斯密斯 的 《 罗马 史 》 ， 对尼禄 和 卡利 古拉 一类 人 ， 已经 有 我 自己 的 看法 。 我 曾 在 心里 暗暗 拿 约翰 和 他们 作 过 比较 ， 可是 从 没想到 会 这样 大声 地说 出来 。"
+    sent2 = u"“ 你 这 男孩 真是 又 恶毒 又 残酷 ！ ” 我 说 。 “ 你 像 个 杀人犯 — — 你 像 个 虐待 奴隶 的 人 — — 你 像 罗马 的 皇帝 ！ ” 我 看过 哥尔 斯密 的 《 罗马 史 》 ， 对尼禄 和 卡里 古拉 等等 ， 已经 有 我 自己 的 看法 。 我 也 默默地 作 过 比较 ， 却 从 没想到 会 大声 地说 出来 。"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    sent1 = u"事实上 ， 我 确实 有点 失常 ， 或者 像 法国人 常说 的 那样 ， 有点儿 不能自制 了 。"
+    sent2 = u"事实上 ， 我 有点儿 失常 ， 或者 像 法国人 所说 的 ， 有点儿 超出 我 自己 的 常规 。"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    sent1 = u"“ 真 不 害臊 ！ 真 不 害臊 ！ ” 使女 嚷嚷 道 ， “ 多 吓人 的 举动 哪 ， 爱 小姐 ， 居然 动手 打 一位 年轻 绅士 ， 打起 你 恩人 的 儿子 ， 你 的 小 主人 来 了 ！ ”"
+    sent2 = u"“ 真 不要脸 ！ 真 不要脸 ！ ” 那 使女 说 。 “ 多 吓人 的 举动 ， 爱 小姐 ， 居然 打 起 年轻 的 绅士 ， 打起 你 恩人 的 儿子 来 了 ！ 居然 打 你 的 小 主人 。 ”"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    sent1 = u"“ 请 爱 小姐 坐下 吧 ， ” 他 说 。 在 他 那 勉强 而 生硬 的 点头 和 不耐烦 但 还 合乎 礼节 的 口气 中 ， 似乎 还 表达 了 另 一层 意思 ： “ 见鬼 ， 爱 小姐 来 没来 跟 我 有 什么 关系 ？ 这会儿 我 才 不 愿意 搭理 她 哩 。 ”"
+    sent2 = u"“ 让 爱 小姐 坐下 吧 ， ” 他 说 。 那 勉强 的 不 自然 的 点头 和 不耐烦 然而 正式 的 语调 中 ， 似乎 有点 什么 东西 要 进一步 表示 ： “ 见鬼 ， 爱 小姐 在 不 在 这儿 ， 和 我 有 什么 关系 ？ 现在 我 可不 愿 招呼 她 。 ”"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    sent1 = u"“ 是 这样 ， ” 那位 好心 的 太太 接应 说 ， 她 现在 明白 我们 在 说 什么 了 ， “ 是 上帝 指引 我 作出 了 这样 的 选择 ， 为 这 我 每天 都 在 感谢 他 。 爱 小姐 是 我 十分 难得 的 伙伴 ， 她 也 是 阿黛尔 和善 细心 的 老师 。 ”"
+    sent2 = u"“ 是 的 ， ” 这位 善良 的 妇人 说 ， 她 现在 知道 我们 在 谈 什么 了 ， “ 上帝 引导 我作 了 这个 选择 ， 我 天天 都 在 感谢 。 爱 小姐 对 我 来说 ， 是 个 非常 可贵 的 伴侣 ， 对 阿黛勒 来说 ， 是 个 既 和蔼 又 细心 的 老师 。 ”"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    # 这个感觉没办法，新旧算法结果都是 0-3, 1-1, 2-2
+    sent1 = u"“ 先生 ？ ” 费尔法克斯 太太 说 。 “ 我 扭伤 了 脚 也 得 感谢 她 哩 。 ”"
+    sent2 = u"“ 是 吗 ？ ” 菲尔 费 克斯 太太 说 。 “ 我 这次 扭伤 了 筋 ， 还 得 谢谢 她 呢 。 ”"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    sent1 = u"“ 因为 是 在 假期 ， 我 没有 别的 事要 做 ， 所以 我 坐在 那儿 从 早上 一直 画到 中午 ， 从 中午 一直 画到 晚上 。 仲夏 的 白天 很长 ， 能 让 我 专心致志 地 工作 。 ”"
+    sent2 = u"“ 我 没有 别的 事 可 做 ， 因为 那 时候 是 假期 ， 我 就 坐 着 从 早上 画到 中午 ， 又 从 中午 画到 晚上 ， 仲夏 白天 很长 ， 对 我 要 埋头工作 的 心情 是 有利 的 。 ”"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    sent1 = u"“ 都 九点 了 。 你 是 怎么 搞 的 ， 爱 小姐 ， 让 阿黛尔 坐 得 这么久 ？ 快带 她 去 睡觉 。 ”"
+    sent2 = u"“ 九点 了 ， 爱 小姐 ， 你 让 阿黛勒 坐 这么久 ， 究竟 是 干什么 ？ 带 她 去 睡觉 。 ”"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    sent1 = u"“ 啊 ！ 我 敢肯定 ！ 你 这人 有点儿 特别 ！ ” 他 说 ， “ 你 的 样子 就 像 个 ‘ 小 修女 ’ 似的 ， 古怪 、 安静 、 严肃 而 又 单纯 。 你 坐在 那儿 ， 两手 放在 身前 ， 眼睛 老是 盯 着 地毯 （ 顺便 说 一句 ， 除了 有时 一个劲儿 盯 着 我 的 脸 ， 比如说 就 像 刚才 那样 ） 。"
+    sent2 = u"“ 啊 ！ 我 敢肯定 ！ 你 这人 有点 特别 ， ” 他 说 ， “ 你 的 样子 就 像 个 nonnette 。 你 坐在 那里 ， 两只手 放在 前面 ， 眼睛 老是 盯 着 地毯 （ 顺便 提 一下 ， 除了 尖利 地 盯 着 我 的 脸 ， 譬如说 就 像 刚才 那样 ） ， 你 显得 古怪 、 安静 、 庄严 和 单纯 。"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    sent1 = u"“ 我 相信 ， 先生 ， 我 决不会 把 不拘礼节 错 当成 傲慢无礼 的 。 前 一种 我 反倒 喜欢 ， 而后 一种 ， 没有 哪个 生来 自由 的 人肯 低头 忍受 的 ， 哪怕 是 看 在 薪水 的 分上 。 ”"
+    sent2 = u"“ 我 肯定 ， 先生 ， 我 决不会 把 不拘礼节 错 认为 蛮横无理 ； 前者 我 是 相当 喜欢 的 ， 后者 ， 却是 任何 一个 自由民 都 不愿 忍受 的 ， 哪怕 是 拿 了 薪俸 ， 也 不愿 忍受 。 ”"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    sent1 = u"可是 ， 我 的 春天 已经 逝去 了 ， 而 把 一朵 法国 小花 留在 了 我 的 手上 。"
+    sent2 = u"不管 怎么样 ， 我 的 春天 已经 过去 了 ， 可是 ， 却 把 那朵 法国 小花 留在 我 手上 。"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    # 匹配结果多了一个 1-5，很奇怪。怀疑是"有意思"词向量没训好
+    sent1 = u"费尔法克斯 太太 在 大厅 里 和 仆人 说话 的 声音 惊醒 了 你 ， 当时 你 多么 奇怪 地 脸露 微笑 ， 而且 在 笑 你 自己 ， 简妮特 ！ 你 的 微笑 意味深长 ， 非常 尖刻 ， 似乎 在 讥笑 你 自己 的 想入非非 。"
+    sent2 = u"菲尔 费 克斯 太太 在 大厅 里 跟 用人 说话 的 声音 把 你 惊醒 ； 你 多么 奇怪 地 对 自己 微笑 ， 而且 笑 你 自己 啊 ， 简 ！ 你 的 微笑 ， 很 有意思 ； 笑 得 很 机灵 ， 似乎 在 嘲笑 你 自己 想 得出 神 。"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    sent1 = u"“ 你 不 把 一切 都 告诉 我 ， 你 就 肯定 走 不了 ！ ” 我 说 。 “ 我 不太想 现在 就 说 。 ”"
+    sent2 = u"“ 你 不 把 一切 都 告诉 我 ， 你 肯定 就 不能 走 ！ ” 我 说 。 “ 现在 我 倒 不想 说 。 ”"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    # 太难：第一步空配找错，后来纠正反而把对的纠错了
+    sent1 = u"他 愿意 把 它 留给 谁 就 留给 谁 ， 现在 他 把 它 留给 了 你 ， 总之 ， 你 拥有 它 是 完全 正当合理 的 ， 你 可以 问心无愧 地 认为 它 完全 属于 你 。 ”"
+    sent2 = u"公正 毕竟 还是 允许 你 保留 它 的 ； 你 可以 问心无愧 地 认为 它 完全 属于 你 。 ”"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    sent1 = u"他 的 野心 是 崇高 的 主 的 精神 那种 野心 ， 它 的 目标 是 要 在 那些 被 拯救 出 尘世 的 人们 的 最 前列 占有 一个 位置 — — 这些 人带 着 无罪 之身 站 在 上帝 的 宝座 跟前 ， 共同 分享 耶稣 最后 的 伟大胜利 ， 他们 都 是 被 召唤 、 被 选中 的 人 ， 都 是 忠贞不渝 的 人 。"
+    sent2 = u"他 的 野心 是 崇高 的 主 的 精神 的 那种 野心 ， 它 的 目的 是 要 在 那些 受到 拯救 离开 尘世 的 人们 中间 的 第一排 上 占 一个 位置 — — 他们 毫无 罪过 地站 在 上帝 的 宝座 跟前 ， 共享 着 耶稣 最后 的 伟大胜利 ， 他们 都 是 被 召唤 、 被 选中 的 人 ， 而且 也 都 是 忠诚 的 人 。"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    sent1 = u"他 也许 依然 严厉 ， 他 也许 依然 苛刻 ， 他 也许 依然 野心勃勃 ， 可是 他 的 严厉 是 武士 大心 的 严厉 ， 正是 大心 保卫 他 护送 的 香客 免受 亚坡伦 的 袭击 。"
+    sent2 = u"他 也许 是 严厉 的 ， 他 也许 是 苛刻 的 ， 他 也许 还是 野心勃勃 的 ； 可是 ， 他 的 严厉 是 武士 大心 的 那种 严厉 ， 正是 大心 保卫 着 他 所 护送 的 香客 不 受亚 玻伦 的 袭击 。"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    # 后面的 \mathbb{N} 型匹配很难解
+    sent1 = u"他 的 苛刻 是 使徒 的 苛刻 ， 使徒 只是 代表 上帝 才 说 ： “ 若 有人 要 跟 从 我 ， 就 当 舍己 ， 背起 他 的 十字架 来 跟 从 我 。 ”"
+    sent2 = u"他 的 苛刻 是 使徒 的 苛刻 ， 使徒 只是 为了 上帝 才 说 ： “ 要 跟着 我 的 人 都 要 拋开 自己 ， 拿 起 他 的 十字架 ， 跟 随着 我 。 ”"
+    print("=" * 100)
+    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    print("refined result: " + matching_result)
+
+    # sent1 = u""
+    # sent2 = u""
+    # print("=" * 100)
+    # matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=verbose_option)
+    # print("refined result: " + matching_result)
+
+
 if __name__ == "__main__":
     vectors = load_word_embedding(os.path.join(args.data_dir, args.embedding))
     normalize = True
@@ -339,98 +481,8 @@ if __name__ == "__main__":
         vectors.syn0 = vectors.syn0 / norms
     print(vectors.syn0.shape)
 
+    unit_test()
 
-    # sent1 = u"里德 太太 对此 则 完全 装聋作哑 ， 她 从 来看 不见 他 打 我 ， 也 从来 听不见 他 骂 我 ， 虽然 他 经常 当着 她 的 面 打 我 骂 我 。"
-    # sent2 = u"里德 太太 呢 ， 在 这种 事情 上 ， 总是 装聋作哑 ， 她 从 来看 不见 他 打 我 ， 也 从来 听不见 他 骂 我 ， 虽然 他 常常 当着 她 的 面 既 打 我 又 骂 我 。"
-    # print("=" * 100)
-    # matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    # print("refined result: " + matching_result)
-    #
-    # sent1 = u"“ 你 这个 狠毒 的 坏孩子 ！ ” 我 说 ， “ 你 简直 像 个 杀人犯 … … 你 是 个 管 奴隶 的 监工 … … 你 像 那班 罗马 暴君 ！ ” 我 看过 哥 尔德 斯密斯 的 《 罗马 史 》 ， 对尼禄 和 卡利 古拉 一类 人 ， 已经 有 我 自己 的 看法 。 我 曾 在 心里 暗暗 拿 约翰 和 他们 作 过 比较 ， 可是 从 没想到 会 这样 大声 地说 出来 。"
-    # sent2 = u"“ 你 这 男孩 真是 又 恶毒 又 残酷 ！ ” 我 说 。 “ 你 像 个 杀人犯 — — 你 像 个 虐待 奴隶 的 人 — — 你 像 罗马 的 皇帝 ！ ” 我 看过 哥尔 斯密 的 《 罗马 史 》 ， 对尼禄 和 卡里 古拉 等等 ， 已经 有 我 自己 的 看法 。 我 也 默默地 作 过 比较 ， 却 从 没想到 会 大声 地说 出来 。"
-    # print("=" * 100)
-    # matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    # print("refined result: " + matching_result)
-    #
-    # sent1 = u"事实上 ， 我 确实 有点 失常 ， 或者 像 法国人 常说 的 那样 ， 有点儿 不能自制 了 。"
-    # sent2 = u"事实上 ， 我 有点儿 失常 ， 或者 像 法国人 所说 的 ， 有点儿 超出 我 自己 的 常规 。"
-    # print("=" * 100)
-    # matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    # print("refined result: " + matching_result)
-    #
-    # sent1 = u"“ 真 不 害臊 ！ 真 不 害臊 ！ ” 使女 嚷嚷 道 ， “ 多 吓人 的 举动 哪 ， 爱 小姐 ， 居然 动手 打 一位 年轻 绅士 ， 打起 你 恩人 的 儿子 ， 你 的 小 主人 来 了 ！ ”"
-    # sent2 = u"“ 真 不要脸 ！ 真 不要脸 ！ ” 那 使女 说 。 “ 多 吓人 的 举动 ， 爱 小姐 ， 居然 打 起 年轻 的 绅士 ， 打起 你 恩人 的 儿子 来 了 ！ 居然 打 你 的 小 主人 。 ”"
-    # print("=" * 100)
-    # matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    # print("refined result: " + matching_result)
-    #
-    # sent1 = u"“ 请 爱 小姐 坐下 吧 ， ” 他 说 。 在 他 那 勉强 而 生硬 的 点头 和 不耐烦 但 还 合乎 礼节 的 口气 中 ， 似乎 还 表达 了 另 一层 意思 ： “ 见鬼 ， 爱 小姐 来 没来 跟 我 有 什么 关系 ？ 这会儿 我 才 不 愿意 搭理 她 哩 。 ”"
-    # sent2 = u"“ 让 爱 小姐 坐下 吧 ， ” 他 说 。 那 勉强 的 不 自然 的 点头 和 不耐烦 然而 正式 的 语调 中 ， 似乎 有点 什么 东西 要 进一步 表示 ： “ 见鬼 ， 爱 小姐 在 不 在 这儿 ， 和 我 有 什么 关系 ？ 现在 我 可不 愿 招呼 她 。 ”"
-    # print("=" * 100)
-    # matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    # print("refined result: " + matching_result)
-    #
-    # sent1 = u"“ 是 这样 ， ” 那位 好心 的 太太 接应 说 ， 她 现在 明白 我们 在 说 什么 了 ， “ 是 上帝 指引 我 作出 了 这样 的 选择 ， 为 这 我 每天 都 在 感谢 他 。 爱 小姐 是 我 十分 难得 的 伙伴 ， 她 也 是 阿黛尔 和善 细心 的 老师 。 ”"
-    # sent2 = u"“ 是 的 ， ” 这位 善良 的 妇人 说 ， 她 现在 知道 我们 在 谈 什么 了 ， “ 上帝 引导 我作 了 这个 选择 ， 我 天天 都 在 感谢 。 爱 小姐 对 我 来说 ， 是 个 非常 可贵 的 伴侣 ， 对 阿黛勒 来说 ， 是 个 既 和蔼 又 细心 的 老师 。 ”"
-    # print("=" * 100)
-    # matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    # print("refined result: " + matching_result)
-    #
-    # # 这个感觉没办法……
-    # sent1 = u"“ 先生 ？ ” 费尔法克斯 太太 说 。 “ 我 扭伤 了 脚 也 得 感谢 她 哩 。 ”"
-    # sent2 = u"“ 是 吗 ？ ” 菲尔 费 克斯 太太 说 。 “ 我 这次 扭伤 了 筋 ， 还 得 谢谢 她 呢 。 ”"
-    # print("=" * 100)
-    # matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    # print("refined result: " + matching_result)
-    #
-    #
-    # sent1 = u"“ 因为 是 在 假期 ， 我 没有 别的 事要 做 ， 所以 我 坐在 那儿 从 早上 一直 画到 中午 ， 从 中午 一直 画到 晚上 。 仲夏 的 白天 很长 ， 能 让 我 专心致志 地 工作 。 ”"
-    # sent2 = u"“ 我 没有 别的 事 可 做 ， 因为 那 时候 是 假期 ， 我 就 坐 着 从 早上 画到 中午 ， 又 从 中午 画到 晚上 ， 仲夏 白天 很长 ， 对 我 要 埋头工作 的 心情 是 有利 的 。 ”"
-    # print("=" * 100)
-    # matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    # print("refined result: " + matching_result)
-    #
-    # sent1 = u"“ 都 九点 了 。 你 是 怎么 搞 的 ， 爱 小姐 ， 让 阿黛尔 坐 得 这么久 ？ 快带 她 去 睡觉 。 ”"
-    # sent2 = u"“ 九点 了 ， 爱 小姐 ， 你 让 阿黛勒 坐 这么久 ， 究竟 是 干什么 ？ 带 她 去 睡觉 。 ”"
-    # print("=" * 100)
-    # matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    # print("refined result: " + matching_result)
-    #
-    # sent1 = u"“ 啊 ！ 我 敢肯定 ！ 你 这人 有点儿 特别 ！ ” 他 说 ， “ 你 的 样子 就 像 个 ‘ 小 修女 ’ 似的 ， 古怪 、 安静 、 严肃 而 又 单纯 。 你 坐在 那儿 ， 两手 放在 身前 ， 眼睛 老是 盯 着 地毯 （ 顺便 说 一句 ， 除了 有时 一个劲儿 盯 着 我 的 脸 ， 比如说 就 像 刚才 那样 ） 。"
-    # sent2 = u"“ 啊 ！ 我 敢肯定 ！ 你 这人 有点 特别 ， ” 他 说 ， “ 你 的 样子 就 像 个 nonnette 。 你 坐在 那里 ， 两只手 放在 前面 ， 眼睛 老是 盯 着 地毯 （ 顺便 提 一下 ， 除了 尖利 地 盯 着 我 的 脸 ， 譬如说 就 像 刚才 那样 ） ， 你 显得 古怪 、 安静 、 庄严 和 单纯 。"
-    # print("=" * 100)
-    # matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    # print("refined result: " + matching_result)
-    #
-    # sent1 = u"“ 我 相信 ， 先生 ， 我 决不会 把 不拘礼节 错 当成 傲慢无礼 的 。 前 一种 我 反倒 喜欢 ， 而后 一种 ， 没有 哪个 生来 自由 的 人肯 低头 忍受 的 ， 哪怕 是 看 在 薪水 的 分上 。 ”"
-    # sent2 = u"“ 我 肯定 ， 先生 ， 我 决不会 把 不拘礼节 错 认为 蛮横无理 ； 前者 我 是 相当 喜欢 的 ， 后者 ， 却是 任何 一个 自由民 都 不愿 忍受 的 ， 哪怕 是 拿 了 薪俸 ， 也 不愿 忍受 。 ”"
-    # print("=" * 100)
-    # matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    # print("refined result: " + matching_result)
-
-    sent1 = u"可是 ， 我 的 春天 已经 逝去 了 ， 而 把 一朵 法国 小花 留在 了 我 的 手上 。"
-    sent2 = u"不管 怎么样 ， 我 的 春天 已经 过去 了 ， 可是 ， 却 把 那朵 法国 小花 留在 我 手上 。"
-    print("=" * 100)
-    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    print("refined result: " + matching_result)
-
-    sent1 = u"费尔法克斯 太太 在 大厅 里 和 仆人 说话 的 声音 惊醒 了 你 ， 当时 你 多么 奇怪 地 脸露 微笑 ， 而且 在 笑 你 自己 ， 简妮特 ！ 你 的 微笑 意味深长 ， 非常 尖刻 ， 似乎 在 讥笑 你 自己 的 想入非非 。"
-    sent2 = u"菲尔 费 克斯 太太 在 大厅 里 跟 用人 说话 的 声音 把 你 惊醒 ； 你 多么 奇怪 地 对 自己 微笑 ， 而且 笑 你 自己 啊 ， 简 ！ 你 的 微笑 ， 很 有意思 ； 笑 得 很 机灵 ， 似乎 在 嘲笑 你 自己 想 得出 神 。"
-    print("=" * 100)
-    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    print("refined result: " + matching_result)
-
-    sent1 = u"“ 你 不 把 一切 都 告诉 我 ， 你 就 肯定 走 不了 ！ ” 我 说 。 “ 我 不太想 现在 就 说 。 ”"
-    sent2 = u"“ 你 不 把 一切 都 告诉 我 ， 你 肯定 就 不能 走 ！ ” 我 说 。 “ 现在 我 倒 不想 说 。 ”"
-    print("=" * 100)
-    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    print("refined result: " + matching_result)
-
-    sent1 = u"他 愿意 把 它 留给 谁 就 留给 谁 ， 现在 他 把 它 留给 了 你 ， 总之 ， 你 拥有 它 是 完全 正当合理 的 ， 你 可以 问心无愧 地 认为 它 完全 属于 你 。 ”"
-    sent2 = u"公正 毕竟 还是 允许 你 保留 它 的 ； 你 可以 问心无愧 地 认为 它 完全 属于 你 。 ”"
-    print("=" * 100)
-    matching_result, s1_clauses, s2_clauses = align_sentence_pair(vectors, sent1, sent2, verbose=True)
-    print("refined result: " + matching_result)
     # align_all_corpus(vectors,
     #                  os.path.join(args.data_dir, args.corpus1),
     #                  os.path.join(args.data_dir, args.corpus2),
